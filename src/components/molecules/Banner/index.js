12bdf9ba20f9f6e78c3418c75d0b0cb4df36043b
Aashish-002 <aashishcoc007@gmail.com>
2024-12-09 20:18:45 +0530

    fix():pdf link changed

--- src/components/molecules/Banner/index.js
+++ src/components/molecules/Banner/index.js
@@ -50,7 +50,7 @@ const Banner = () => {
 
         <div className="flex items-center translate-y-[-60%] sm:translate-y-[-0%]">
           <a
-            href="https://drive.google.com/file/d/19rnbukAhf9oPhadMhsvI3xnWF6FIYeMT/view?usp=share_link"
+            href="https://drive.google.com/file/d/1OVASjpCfqEBPMbGZVAnxyuVmX1HkzB8d/view?usp=sharing"
             target="blank"
           >
             <button className="primary-button">
