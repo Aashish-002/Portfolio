db88fe23b8cf7733536349b671bea067589c7a09
Aashish-002 <aashishcoc007@gmail.com>
2024-12-09 20:06:45 +0530

    feat():adding of files

---
+++ src/pages/About/index.js
@@ -0,0 +1,121 @@
+import React from "react";
+import TypeAnimation from "react-type-animation";
+import { motion } from "framer-motion";
+import "./About.css";
+import "../../components/atoms/PrimaryBtn/PrimaryBtn.css";
+import "../shared/Shared.css";
+import { BottomLine, MySkill, Education } from "../../components";
+import { FaDownload } from "react-icons/fa";
+
+const About = () => {
+  return (
+    <div className="parent pt-16 my-16">
+      <div className="">
+        <motion.div
+          className="mb-10"
+          initial={{ y: -200, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { duration: 1, type: "spring" },
+          }}
+        >
+          <h3 className="text-neutral text-center">Something About Myself</h3>
+          <h1 className="text-4xl font-semibold drop-shadow-md text-center">
+            About <span className="text-primary">Me</span>
+          </h1>
+          <BottomLine />
+        </motion.div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
+          <motion.div
+            initial={{ x: -200, opacity: 0 }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              transition: { duration: 1, delay: 1.25 },
+            }}
+          >
+            <img
+              src="/logoB.png"
+              alt="Aashish P"
+              className="p-12 w-70 h-70 transform translate-y-[-12%]"
+              title="Aashish P"
+            />
+
+          </motion.div>
+          <motion.div
+            className=""
+            initial={{ x: 200, opacity: 0 }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              transition: { duration: 1, delay: 1.25 },
+            }}
+          >
+            <h1 className="text-4xl font-semibold mb-4 text-center translate-y-[-380%] sm:translate-y-[-0%] sm:text-3xl sm:mb-2 md:text-left">Aashish P</h1>
+            <div className="my-8">
+              <TypeAnimation
+                className="text-2xl text-primary font-bold text-center translate-y-[-500%] sm:translate-y-[-0%]  sm:text-2xl sm:mb-2 md:text-left"
+                cursor={true}
+                sequence={[
+                  "A Mern-stack Developer",
+                  2000,
+                  "A Full-stack Developer",
+                  2000,
+                  "A Front-end Developer",
+                  2000
+                ]}
+                wrapper="div"
+                repeat={Infinity}
+              />
+            </div>
+            <p className="text-neutral font-medium text-center translate-y-[-100%] sm:translate-y-[-0%] sm:mb-2 md:text-left">
+            As a MERN stack developer, I am passionate about creating high-quality web applications designed to meet and exceed client expectations. With extensive experience in full-stack web development, I specialize in leveraging modern technologies such as React.js, Next.js, TypeScript, MongoDB, Express.js, and Node.js to build scalable, efficient, and robust solutions. My expertise lies in delivering applications that combine seamless user experiences with powerful, reliable backend architectures.
+            </p>
+            <br />
+            <p className="text-neutral font-medium text-center translate-y-[-150%] sm:translate-y-[-0%] sm:mb-2 md:text-left">
+            Driven by a passion for solving complex challenges and delivering innovative solutions, I remain committed to staying at the forefront of industry trends and advancements in technology. This dedication enables me to continuously enhance my skills and bring cutting-edge expertise to every project I undertake.
+            </p>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-4 mt-4 text-center translate-y-[-50%] sm:translate-y-[-0%] sm:mb-2 md:text-left ">
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Name : </span>Aashish P
+              </h2>
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Phone : </span>+91 73580 85923
+              </h2>
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Email : </span>
+                paashish.work@gmail.com
+              </h2>
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Address : </span>Chennai, Tamil Nadu, India
+              </h2>
+              <a
+                href="https://drive.google.com/file/d/1OVASjpCfqEBPMbGZVAnxyuVmX1HkzB8d/view?usp=sharing"
+                target="blank"
+              >
+                <div className="flex justify-center md:justify-start translate-y-[60%] sm:translate-y-[-0%]">
+                  <button className="primary-button">
+                    <span>My Resume</span>
+                    <span>
+                      <FaDownload />
+                    </span>
+                  </button>
+                </div>
+
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+
+      {/* My Skill */}
+      <MySkill />
+
+      {/* Education */}
+      <Education />
+    </div>
+  );
+};
+
+export default About;
